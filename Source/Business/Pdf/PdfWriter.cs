f312627e557f6b2a3a7d4d3b29c237eabe63f048
radasi <101691143+radasi@users.noreply.github.com>
2022-12-08 13:05:23 -0300

    Atualização do HabitaSorte CasaNova - Jd. Itanguá
    4 faixas de renda:
    - Faixa A - 0 a 3 salários mínimos;
    - Faixa B - 0 a 3 salários mínimos;
    - Faixa C - 3 a 5 salários mínimos;
    - Faixa D - 5 a 7 salários mínimos;
    4 critérios de socioeconômicos:
    - coabitação familiar;
    - adensamento excessivo em domicilio alugado;
    - ônus excessivo com aluguel;
    - grupos familiares com pelo menos 02 membros;

--- Source/Business/Pdf/PdfWriter.cs
+++ Source/Business/Pdf/PdfWriter.cs
@@ -14,6 +14,8 @@ namespace Habitasorte.Business.Pdf {
 
         internal static void WriteToPdf(string caminhoArquivo, Sorteio sorteio, ListaPub lista)
         {
+            DateTime dataHoraImpressao = new DateTime();
+            List<String> linhas = new List<String>();
             using (FileStream fileStream = new FileStream(caminhoArquivo, FileMode.Create))
             {
                 PdfWriter writer = null;
@@ -25,14 +27,19 @@ namespace Habitasorte.Business.Pdf {
                     writer = PdfWriter.GetInstance(document, fileStream);
                     writer.PageEvent = new CustomPdfPageEventHelper(lista.Nome);
                     document.Open();
+                    dataHoraImpressao = DateTime.Now;
 
                     Font headerFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 12);
                     Paragraph p = new Paragraph();
                     p.Alignment = Element.ALIGN_CENTER;
                     p.Font = headerFont;
+                    linhas.Add("CASA NOVA - PREFEITURA DE SOROCABA \n");
                     p.Add("CASA NOVA - PREFEITURA DE SOROCABA \n");
+                    linhas.Add(string.Format("{0}\n", sorteio.Nome.ToUpper()));
                     p.Add(string.Format("{0}\n", sorteio.Nome.ToUpper()));
+                    linhas.Add(string.Format("{0:00} - {1}\n", lista.IdLista, lista.Nome.ToUpper()));
                     p.Add(string.Format("{0:00} - {1}\n", lista.IdLista, lista.Nome.ToUpper()));
+                    linhas.Add(string.Format("Semente de Sorteio: {0} ({1})\n\n", lista.SementeSorteio, lista.FonteSementeSorteio));
                     p.Add(string.Format("Semente de Sorteio: {0} ({1})\n\n", lista.SementeSorteio, lista.FonteSementeSorteio));
                     document.Add(p);
 
@@ -72,16 +79,44 @@ namespace Habitasorte.Business.Pdf {
                         BackgroundColor = BaseColor.LIGHT_GRAY
                     });
 
+                    linhas.Add(String.Concat("Nº   ", "CPF                 ", "NOME,", "CRITÉRIOS,", "INSCRIÇÃO"));
+
                     foreach (CandidatoPub candidato in lista.Candidatos)
                     {
                         table.AddCell(string.Format("{0:000}", candidato.IdCandidato));
-                        table.AddCell(string.Format("{0:000'.'000}", candidato.Cpf.ToString().Substring(0, 7)) + ".***-**");
+                        table.AddCell(string.Format("***.{0:000'.'000}", candidato.Cpf.ToString().Substring(4, 7)) + "-**");
                         table.AddCell(candidato.Nome.ToUpper());
                         table.AddCell(candidato.QuantidadeCriterios.ToString());
                         table.AddCell(string.Format("{0:000000}", candidato.IdInscricao.ToString()));
+                        linhas.Add(String.Concat(string.Format("{0:000}", candidato.IdCandidato), " ", string.Format("***.{0:000'.'000'}", candidato.Cpf.ToString().Substring(4, 7)) + "-**", " ", candidato.Nome.ToUpper(), ", ", candidato.QuantidadeCriterios.ToString(), ", ", string.Format("{0:000000}", candidato.IdInscricao.ToString())));
                     }
 
                     document.Add(table);
+                }
+                System.Drawing.Printing.PrintDocument printer = new System.Drawing.Printing.PrintDocument();
+                printer.PrintPage += delegate (object envio, System.Drawing.Printing.PrintPageEventArgs eArgs)
+                {
+                    for (int ix = 0; ix < linhas.Count; ix++)
+                    {
+                        if (ix < 4)
+                        {
+                            eArgs.Graphics.DrawString(linhas[ix], new System.Drawing.Font("HELVETICA_BOLD", 8, System.Drawing.FontStyle.Bold), new System.Drawing.SolidBrush(System.Drawing.Color.Black), new System.Drawing.PointF(300 - (linhas[ix].Length), ix * 15));
+                        } else
+                        {
+                            eArgs.Graphics.DrawString(linhas[ix].Split(',')[0], new System.Drawing.Font("HELVETICA_BOLD", 8), new System.Drawing.SolidBrush(System.Drawing.Color.Black), new System.Drawing.RectangleF(0, ix * 15, 650, 20));
+                            eArgs.Graphics.DrawString(linhas[ix].Split(',')[1], new System.Drawing.Font("HELVETICA_BOLD", 8), new System.Drawing.SolidBrush(System.Drawing.Color.Black), new System.Drawing.RectangleF(650, ix * 15, 100, 20));
+                            eArgs.Graphics.DrawString(linhas[ix].Split(',')[2], new System.Drawing.Font("HELVETICA_BOLD", 8), new System.Drawing.SolidBrush(System.Drawing.Color.Black), new System.Drawing.RectangleF(750, ix * 15, 100, 20));
+                        }
+                    }
+                    eArgs.Graphics.DrawString(lista.Nome, new System.Drawing.Font("HELVETICA_BOLD", 8), new System.Drawing.SolidBrush(System.Drawing.Color.Gray), new System.Drawing.RectangleF(0, 1080, 700, 20));
+                    eArgs.Graphics.DrawString(dataHoraImpressao.ToString(), new System.Drawing.Font("HELVETICA_BOLD", 8), new System.Drawing.SolidBrush(System.Drawing.Color.Gray), new System.Drawing.RectangleF(700, 1080, 200, 20));
+                };
+                try
+                {
+                    printer.Print();
+                } catch(Exception ex)
+                {
+
                 }
                 writer.Close();
             }
@@ -144,11 +179,18 @@ namespace Habitasorte.Business.Pdf {
                         HorizontalAlignment = 1,
                         BackgroundColor = BaseColor.LIGHT_GRAY
                     });
-
+                    
                     foreach (CandidatoPub candidato in lista.Candidatos) {
-                        table.AddCell(new Phrase(string.Format("{0:000'.'000}", candidato.Cpf.ToString().Substring(0, 7)) + ".***-**", headerFont));
+                        table.AddCell(new Phrase(string.Format("***.{0:000'.'000'}", candidato.Cpf.ToString().Substring(4, 7)) + "-**", headerFont));
                         table.AddCell(new Phrase(candidato.Nome.ToUpper(), headerFont));
                         table.AddCell(new Phrase(string.Format("{0:000000}", candidato.IdInscricao.ToString()), headerFont));
+                    }
+
+                    if (lista.Candidatos.Count % 2 > 0)
+                    {
+                        table.AddCell(new Phrase(String.Empty));
+                        table.AddCell(new Phrase(String.Empty));
+                        table.AddCell(new Phrase(String.Empty));
                     }
 
                     document.Add(table);
